8571edfc5f7d582dee9defe44e70b3606303767d
front.sergey.o <front.sergey.o@alldigitalads.com>
2019-01-26 14:26:19 +0200

    add macros, if, each

--- app/js/components/functions.js
+++ app/js/components/functions.js
@@ -14,3 +14,18 @@ function sayHello() {
     }
 }
 
+$(document).ready(function () {
+
+
+    $('.slides__wrapper').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        arrows: false,
+        cssEase: 'linear',
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+});
